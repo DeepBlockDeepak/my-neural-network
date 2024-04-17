e1fd97abe49a836febed233dee42ed54969fae1a
DeepBlockDeepak <jordan.medina1729@gmail.com>
2024-04-16 23:32:51 -0600

    New CI workflow file, testing subdirect, and modularized compute_loss and forward_prop depending on multi class or binary class

--- src/my_neural_network/neuron.py
+++ src/my_neural_network/neuron.py
@@ -56,77 +56,12 @@ class SimpleNeuralNetwork:
 
     def compute_loss(self, AL: np.ndarray, Y: np.ndarray) -> float:
         """
-        Computes the categorical cross-entropy loss.
-
-        Cross-entropy -> loss function for multi-class classification problems.
-        It measures the performance of a classification model whose output is a probability value between 0 and 1.
-
-        Args:
-            AL (np.ndarray): The activation layer... the probabilities predicted by the model for each class and example,
-                             i.e., the output of the last layer's activation function.
-            Y (np.ndarray): The true labels of the data in one-hot encoded form.
-
-        Returns:
-            float: The average cross-entropy loss across all examples in the dataset.
-        """
-        # Categorical cross-entropy computation
-        m = Y.shape[1]  # Number of samples
-        cost = (
-            -np.sum(Y * np.log(AL + 1e-8)) / m
-        )  # 1e-8 is added for numerical stability to avoid log(0)
-        cost = np.squeeze(cost)  # Reduce the dimensionality of the cost to a scalar
-        return cost
-
-    def forward_propagation(self, X: np.ndarray) -> tuple:
-        """
-        Performs the forward pass through the network.
-        Computes the activation at each layer by applying the linear transformation then the activation function.
-
-        Iterates over all layers except the last one using ReLU activation.
-        Applies softmax activation at the final layer to output probabilities for classification.
-
-        Args:
-            X (np.ndarray): Input data (features) where each column represents an example.
-
-        Returns:
-            tuple: A tuple containing:
-                - AL (np.ndarray): The softmax probabilities of the output layer, which are the
-                                   predicted probabilities for each class for each example.
-                - caches (list): A list of tuples, where each tuple contains:
-                                 (A_prev, W, b, Z) for each layer. These are cached for
-                                 use in the backward pass:
-                                 - A_prev: activations from the previous layer
-                                 - W: weights matrix of the current layer
-                                 - b: bias vector of the current layer
-                                 - Z: linear component (W*A_prev + b) of the current layer
-        """
-        caches = []
-        A = X
-        L = self.L - 1  # Number of layers excluding the output layer
-
-        # Iterate through each layer up to the second to last layer to apply ReLU activation
-        for l in range(1, L):
-            A_prev = A
-            W = self.parameters["W" + str(l)]
-            b = self.parameters["b" + str(l)]
-            Z = np.dot(W, A_prev) + b
-            A = np.maximum(0, Z)  # ReLU activation
-            caches.append((A_prev, W, b, Z))
-
-        # Final layer uses a softmax activation function
-        W = self.parameters["W" + str(L)]
-        b = self.parameters["b" + str(L)]
-        Z = np.dot(W, A) + b
-        AL = self.softmax(Z)
-        caches.append((A, W, b, Z))
-
-        return AL, caches
-
-    def compute_loss_binary(self, AL: np.ndarray, Y: np.ndarray) -> float:
-        """
-        Compute the binary cross-entropy loss.
+        Computes the binary cross-entropy loss or categorical cross-entropy loss depending on network type.
 
         Binary cross-entropy is the loss function for binary classification problems.
+        It measures the performance of a classification model whose output is a probability value between 0 and 1.
+
+        Cross-entropy -> loss function for multi-class classification problems.
         It measures the performance of a classification model whose output is a probability value between 0 and 1.
 
         Args:
@@ -137,24 +72,27 @@ class SimpleNeuralNetwork:
             float: The average binary cross-entropy loss across all examples in the dataset.
         """
         m = Y.shape[1]  # Number of samples
-        cost = -np.sum(Y * np.log(AL + 1e-8) + (1 - Y) * np.log(1 - AL + 1e-8)) / m
-        cost = np.squeeze(cost)  # Reduce the dimensionality of the cost to a scalar
-        return cost
+        if Y.shape[0] == 1:  # Binary cross-entropy
+            # 1e-8 is added for numerical stability to avoid log(0)
+            cost = -np.sum(Y * np.log(AL + 1e-8) + (1 - Y) * np.log(1 - AL + 1e-8)) / m
+        else:  # Categorical cross-entropy
+            cost = -np.sum(Y * np.log(AL + 1e-8)) / m
+        return np.squeeze(cost)  # Reduce the dimensionality of the cost to a scalar
 
-    def forward_propagation_binary(self, X: np.ndarray) -> tuple:
+    def forward_propagation(self, X: np.ndarray) -> tuple:
         """
-        Performs the forward pass through the network for binary classification.
+        Performs the forward pass through the network for binary or multiclass classification.
         Computes the activation at each layer by applying the linear transformation followed by the activation function.
 
         Iterates over all layers except the last one using ReLU activation.
-        Applies sigmoid activation at the final layer to output a probability for the binary class.
+        Applies sigmoid or softmax activation at the final layer to output a probability for the binary class or multi-class, respectively.
 
         Args:
             X (np.ndarray): Input data (features) where each column represents an example.
 
         Returns:
             tuple: A tuple containing:
-                - AL (np.ndarray): The sigmoid probability of the output layer, which is the
+                - AL (np.ndarray): The sigmoid or softmax probability of the output layer, which is the
                                 predicted probability for the positive class for each example.
                 - caches (list): A list of tuples, where each tuple contains:
                                 (A_prev, W, b, Z) for each layer. These are cached for
@@ -176,10 +114,18 @@ class SimpleNeuralNetwork:
             A = np.maximum(0, Z)  # ReLU activation
             caches.append((A_prev, W, b, Z))
 
+        # final activation/output layer
         W = self.parameters["W" + str(L)]
         b = self.parameters["b" + str(L)]
         Z = np.dot(W, A) + b
-        AL = self.sigmoid(Z)
+
+        if self.layer_dims[-1] == 1:
+            AL = self.sigmoid(
+                Z
+            )  # Binary classification (single neuron in output layer)
+        else:
+            AL = self.softmax(Z)  # Multi-class classification (more than one neuron)
+
         caches.append((A, W, b, Z))
 
         return AL, caches
@@ -209,13 +155,31 @@ class SimpleNeuralNetwork:
         sig = self.sigmoid(Z)
         return sig * (1 - sig)
 
+    def compute_initial_gradient(self, AL, Y):
+        """
+        Differentiate between binary cross-entropy and multi-class cross-entropy.
+
+        Args:
+            AL (np.ndarray): Probability vector, output of the forward propagation (forward_propagation()).
+            Y (np.ndarray): True "label" vector (for example: containing 0 if non-cat, 1 if cat).
+
+        Returns:
+            dAL (np.ndarray): Initial gradient of the loss with respect to the activation layer.
+        """
+        if (
+            AL.shape[0] == 1
+        ):  # Binary classification - sigmoid activation for binary cross-entropy loss
+            return -(np.divide(Y, AL + 1e-8) - np.divide(1 - Y, 1 - AL + 1e-8))
+        else:  # Multi-class classification- softmax activation
+            return AL - Y
+
     def backward_propagation(self, AL: np.ndarray, Y: np.ndarray, caches: list) -> dict:
         """
         Perform the backward pass using gradient descent to compute the gradients of the loss function
         with respect to the weights and biases.
 
         Args:
-            AL (np.ndarray): Probability vector, output of the forward propagation (L_model_forward()).
+            AL (np.ndarray): Probability vector, output of the forward propagation (forward_propagation()).
             Y (np.ndarray): True "label" vector (for example: containing 0 if non-cat, 1 if cat).
             caches (list): List of caches containing every cache of linear_activation_forward()
                            with "relu" (it's caches[l], for l in range(L-1) i.e., l = 0...L-2).
@@ -232,8 +196,7 @@ class SimpleNeuralNetwork:
         m = AL.shape[1]
         Y = Y.reshape(AL.shape)  # Ensure Y has the same shape as AL
 
-        # Initializing the backpropagation
-        dAL = -(np.divide(Y, AL + 1e-8) - np.divide(1 - Y, 1 - AL + 1e-8))
+        dAL = self.compute_initial_gradient(AL, Y)
 
         # Loop from l=L-1 to l=0
         for l in range(L - 1, 0, -1):
@@ -273,7 +236,30 @@ class SimpleNeuralNetwork:
             self.parameters["W" + str(l)] -= learning_rate * grads["dW" + str(l)]
             self.parameters["b" + str(l)] -= learning_rate * grads["db" + str(l)]
 
-    def train(self, X, Y, iterations, learning_rate):
+    def train(self, X, Y, iterations, learning_rate) -> None:
+        """
+        This method trains the neural network using gradient descent optimization.
+        It iteratively performs forward propagation, computes the loss, backward propagation, and updates the parameters of the network.
+
+        Args:
+            X (np.ndarray): Input data matrix where each column represents a training example.
+            Y (np.ndarray): True label matrix where each column represents the true labels for the corresponding training example in X.
+            iterations (int): Number of iterations for training.
+            learning_rate (float): Learning rate for gradient descent.
+
+        Returns:
+            None
+
+        Notes:
+            During each iteration:
+            - Forward propagation is performed to compute the output predictions (AL) and caches containing intermediate values needed for backward propagation.
+            - The loss is computed based on the output predictions (AL) and true labels (Y).
+            - Backward propagation is performed to compute gradients of the loss with respect to the parameters of the network.
+            - The parameters of the network are updated using gradient descent with the computed gradients and the specified learning rate.
+
+            After every 100 iterations, the current cost (loss) is printed to monitor the training progress.
+
+        """
         for i in range(iterations):
             # Forward propagation
             AL, caches = self.forward_propagation(X)
--- tests/test_neuron.py
+++ tests/test_neuron.py
@@ -1,21 +1,96 @@
 import numpy as np
+import pytest
 
 from src.my_neural_network.neuron import SimpleNeuralNetwork
 
 
-def test_compute_loss():
-    # Setup
-    y_true = np.array([[1, 0, 0]])  # Assuming three classes and one example
-    y_pred = np.array([[0.7, 0.2, 0.1]])  # Sample prediction
+@pytest.fixture(
+    params=[
+        [5, 10, 1],  # Binary classifier configuration
+        [5, 10, 3],  # Multi-class classifier configuration
+    ]
+)
+def create_network(request):
+    layer_dims = request.param
+    network = SimpleNeuralNetwork(layer_dims)
+    return network
 
-    # Expected output
-    expected_loss = -np.log(0.7)  # Only the first class contributes to loss
 
-    # Initialize the network and compute loss
-    net = SimpleNeuralNetwork(layer_dims=[3, 3, 3])
-    loss = net.compute_loss(y_pred, y_true)
+def test_initialization(create_network):
+    network = create_network
+    assert all(
+        key in network.parameters
+        for l in range(1, network.L)
+        for key in (f"W{l}", f"b{l}")
+    ), "Weights and biases are not initialized for all layers."
+    assert all(
+        isinstance(network.parameters[f"W{l}"], np.ndarray)
+        and network.parameters[f"W{l}"].shape
+        == (network.layer_dims[l], network.layer_dims[l - 1])
+        for l in range(1, network.L)
+    ), "Weights are not initialized correctly."
+    assert all(
+        isinstance(network.parameters[f"b{l}"], np.ndarray)
+        and network.parameters[f"b{l}"].shape == (network.layer_dims[l], 1)
+        for l in range(1, network.L)
+    ), "Biases are not initialized correctly."
 
-    # Assert
-    assert np.isclose(
-        loss, expected_loss
-    ), f"The computed loss is incorrect: {loss:.2f} vs {expected_loss:.2f}"
+
+def test_forward_propagation(create_network):
+    network = create_network
+    X = np.random.randn(5, 10)  # Example input (5 features, 10 samples)
+    AL, caches = network.forward_propagation(X)
+    assert AL.shape == (
+        network.layer_dims[-1],
+        X.shape[1],
+    ), "Output layer dimensions are incorrect."
+    assert len(caches) == network.L - 1, "Cache not correctly formed."
+
+
+def test_sigmoid(create_network):
+    network = create_network
+    Z = np.array([0, 2, -2])
+    A = network.sigmoid(Z)
+    assert np.allclose(
+        A, [0.5, 0.88079708, 0.11920292]
+    ), "Sigmoid function is incorrect."
+
+
+def test_softmax(create_network):
+    network = create_network
+    Z = np.array([[0, 1], [2, 1]])
+    A = network.softmax(Z)
+    expected_output = np.array([[0.11920292, 0.5], [0.88079708, 0.5]])
+    assert np.allclose(A, expected_output), "Softmax function is incorrect."
+
+
+def test_backward_propagation(create_network):
+    """
+    Checks that all expected gradients are present in the returned gradients dictionary. 
+    This ensures that the backward propagation logic is functioning correctly 
+    and that it is providing all necessary information for parameter updates.
+    """
+    network = create_network
+    X = np.random.randn(
+        network.layer_dims[0], 10
+    )  # Generates a random input matrix X with a shape of the network’s input layer dimension. 10 samples/batch size
+    num_classes = network.layer_dims[-1]
+
+    # Prepare labels Y based on number of output classes
+    if num_classes == 1:
+        Y = np.random.randint(0, 2, (1, 10))  # single row of Binary labels with vals of [0,1]
+    else: # num_classes is the number of different classes the network can classify.
+        # Generate random labels for multi-class, and convert to one-hot
+        labels = np.random.randint(0, num_classes, (10,))  # Generate random labels
+        Y = np.zeros((num_classes, 10))
+        Y[labels, np.arange(10)] = 1  # Proper one-hot encoding
+        # Together, `labels` and `np.arange(10)` form a set of index pairs 
+        # that specify which elements of the array Y to set to 1. 
+        # For each column  j (ranging from 0 to 9), the element at row labels[j] 
+        # is set to 1. This places a 1 in the position corresponding to the class label for each sample.
+
+    AL, caches = network.forward_propagation(X)
+    grads = network.backward_propagation(AL, Y, caches)
+    assert all(
+        f"dW{l}" in grads and f"db{l}" in grads for l in range(1, network.L)
+    ), "Gradient keys missing."
