0d8c10c2b2db738f736a18fe1c35f368757bb39b
DeepBlockDeepak <jordan.medina1729@gmail.commit>
2024-04-30 17:39:23 -0600

    finished issue

---
+++ scripts/coverage_test.py
@@ -0,0 +1,17 @@
+import subprocess
+
+
+def run():
+    """Runs all tests within the `tests` directory with coverage reporting."""
+    subprocess.run(
+        [
+            "poetry",
+            "run",
+            "pytest",
+            "tests",
+            "--cov=my_neural_network",
+            "--cov-report=term",
+            "--cov-report=html:coverage_html",
+        ],
+        check=True,
+    )
